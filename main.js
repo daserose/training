10655b9b673fea843f6c4ea8db79bb945a7f2abc
yaroslavlesk <yaroslavlesk@gmail.com>
2020-07-14 22:17:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// alert("Hello world");
+
+// let incr = 10,
+//     decr = 10;
+
+    // incr++;
+    // decr--;
+    // console.log(incr);
+    // console.log(decr);
+
+    // ++incr;
+    // --decr;
+    // console.log(incr);
+    // console.log(decr);
+
+// console.log(incr++);
+// console.log(incr);
+
+// const obj = {
+//     name: 'Yaroslav',
+//     age: 32,
+//     isMarried: true
+// };
+
+// // console.log(obj.isMarried)
+// console.log(obj['name']);
+
+// let arr = ['aefqe.png', 'orange.jpg', 6, [], {}];
+// console.log(arr[2]);
+
+// const answer = +prompt('Enter some text', 'some text');
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Name?');
+answers[1] = prompt('Age?');
+answers[2] = prompt('sex?');
+
+document.write(answers);
+
